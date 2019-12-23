d2bcd1719f5b4baed187b2a9b6937e627cf7bb6b
Zad Sixstrings <toxxiczad@gmail.com>
2019-12-23 14:58:24 +0100

    Update wa-cleanup.sh

--- wa-cleanup.sh
+++ wa-cleanup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Deleting WhatsApp sent files..."
-rm -r /Media/"WhatsApp Animated Gifs"/
-rm -r /Media/"WhatsApp Images"/
-rm -r /Media/"WhatsApp Video"/
-rm -r /Media/"WhatsApp Audio"/
-rm -r /Media/"WhatsApp Documents"/
-rm -r /Media/"WhatsApp Stickers"/
-rm -r /Media/"WhatsApp Voice Notes"/
+rm -r /Media/"WhatsApp Animated Gifs"
+rm -r /Media/"WhatsApp Images"
+rm -r /Media/"WhatsApp Video"
+rm -r /Media/"WhatsApp Audio"
+rm -r /Media/"WhatsApp Documents"
+rm -r /Media/"WhatsApp Stickers"
+rm -r /Media/"WhatsApp Voice Notes"
 sleep 1
 echo "Deletion process complete."
