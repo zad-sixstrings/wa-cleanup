4a10d5ee6a8152a3d0be526f39e212e7dbec6bbc
Zad Sixstrings <toxxiczad@gmail.com>
2019-12-23 14:43:40 +0100

    Update wa-cleanup.sh

--- wa-cleanup.sh
+++ wa-cleanup.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 echo "Deleting WhatsApp sent files..."
-rm /sdcard/WhatsApp/Media/WhatsApp Animated Gifs/Sent/*
-rm /sdcard/WhatsApp/Media/WhatsApp Animated Gifs/Private/*
-rm /sdcard/WhatsApp/Media/WhatsApp Images/Sent/*
-rm /sdcard/WhatsApp/Media/WhatsApp Images/Private/*
-rm /sdcard/WhatsApp/Media/WhatsApp Images/*.jpeg
-rm /sdcard/WhatsApp/Media/WhatsApp Video/Sent/*
-rm /sdcard/WhatsApp/Media/WhatsApp Video/Private/*
-rm /sdcard/WhatsApp/Media/WhatsApp Audio/Sent/*
-rm /sdcard/WhatsApp/Media/WhatsApp Audio/Private/*
-rm /sdcard/WhatsApp/Media/WhatsApp Audio/*.opus
-rm /sdcard/WhatsApp/Media/WhatsApp Documents/Sent/*
-rm /sdcard/WhatsApp/Media/WhatsApp Documents/Private/*
-rm /sdcard/WhatsApp/Media/WhatsApp Stickers/*
-rm /sdcard/WhatsApp/Media/WhatsApp Voice Notes/*
+rm -r /sdcard/WhatsApp/Media/"WhatsApp Animated Gifs"/
+rm -r /sdcard/WhatsApp/Media/"WhatsApp Images"/
+rm -r /sdcard/WhatsApp/Media/"WhatsApp Video"/
+rm -r /sdcard/WhatsApp/Media/"WhatsApp Audio"/
+rm -r /sdcard/WhatsApp/Media/"WhatsApp Documents"/
+rm -r /sdcard/WhatsApp/Media/"WhatsApp Stickers"/
+rm -r /sdcard/WhatsApp/Media/"WhatsApp Voice Notes"/
 sleep 1
 echo "Deletion process complete."
