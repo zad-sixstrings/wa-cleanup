421871d985804b4560a5240b39c7f3b774a3cbcc
Zad Sixstrings <toxxiczad@gmail.com>
2019-12-23 14:05:45 +0100

    Update wa-cleanup.sh

--- wa-cleanup.sh
+++ wa-cleanup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 echo "Deleting WhatsApp sent files..."
-rm /sdcard/WhatsApp/Media/"WhatsApp Animated Gifs"/Sent/*
-rm /sdcard/WhatsApp/Media/"WhatsApp Animated Gifs"/Private/*
-rm /sdcard/WhatsApp/Media/"WhatsApp Images"/Sent/*
-rm /sdcard/WhatsApp/Media/"WhatsApp Images"/Private/*
-rm /sdcard/WhatsApp/Media/"WhatsApp Images"/*.jpeg
-rm /sdcard/WhatsApp/Media/"WhatsApp Video"/Sent/*
-rm /sdcard/WhatsApp/Media/"WhatsApp Video"/Private/*
-rm /sdcard/WhatsApp/Media/"WhatsApp Audio"/Sent/*
-rm /sdcard/WhatsApp/Media/"WhatsApp Audio"/Private/*
-rm /sdcard/WhatsApp/Media/"WhatsApp Audio"/*.opus
-rm /sdcard/WhatsApp/Media/"WhatsApp Documents"/Sent/*
-rm /sdcard/WhatsApp/Media/"WhatsApp Documents"/Private/*
-rm /sdcard/WhatsApp/Media/"WhatsApp Stickers"/*
-rm /sdcard/WhatsApp/Media/"WhatsApp Voice Notes"/*
+rm /sdcard/WhatsApp/Media/WhatsApp Animated Gifs/Sent/*
+rm /sdcard/WhatsApp/Media/WhatsApp Animated Gifs/Private/*
+rm /sdcard/WhatsApp/Media/WhatsApp Images/Sent/*
+rm /sdcard/WhatsApp/Media/WhatsApp Images/Private/*
+rm /sdcard/WhatsApp/Media/WhatsApp Images/*.jpeg
+rm /sdcard/WhatsApp/Media/WhatsApp Video/Sent/*
+rm /sdcard/WhatsApp/Media/WhatsApp Video/Private/*
+rm /sdcard/WhatsApp/Media/WhatsApp Audio/Sent/*
+rm /sdcard/WhatsApp/Media/WhatsApp Audio/Private/*
+rm /sdcard/WhatsApp/Media/WhatsApp Audio/*.opus
+rm /sdcard/WhatsApp/Media/WhatsApp Documents/Sent/*
+rm /sdcard/WhatsApp/Media/WhatsApp Documents/Private/*
+rm /sdcard/WhatsApp/Media/WhatsApp Stickers/*
+rm /sdcard/WhatsApp/Media/WhatsApp Voice Notes/*
 sleep 1
 echo "Deletion process complete."
